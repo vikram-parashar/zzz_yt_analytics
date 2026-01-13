ea1890b1f23c352444bfa9dac03c439b891c9955
vikram-parashar <vikramparashar24@gmail.com>
2026-01-13 22:10:19 +0530

    agent upload to bq

---
+++ src/config.py
@@ -0,0 +1,8 @@
+PROJECT_ID = "learning-474319"
+BQ_LOCATION = "asia-south2"
+
+BQ_DATASET = "youtube_analytics"
+
+AGENT_TABLE = "dim_agent"
+
+DISCOVERY_LOOKBACK_DAYS = 90
---
+++ terraform/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "learning-474319"
+  region  = "asia-south2"
+  zone    = "asia-south2-a"
+}
+
+resource "google_bigquery_dataset" "default" {
+  dataset_id = "youtube_analytics"
+  location   = "EU"
+}
+
+resource "google_bigquery_table" "dim_agent" {
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = "dim_agent"
+
+  deletion_protection = true
+
+  schema = jsonencode([
+    {
+      name = "icon"
+      type = "STRING"
+      mode = "NULLABLE"
+    },
+    {
+      name = "name"
+      type = "STRING"
+      mode = "NULLABLE"
+    },
+    {
+      name = "rank"
+      type = "STRING"
+      mode = "NULLABLE"
+    },
+    {
+      name = "attribute"
+      type = "STRING"
+      mode = "NULLABLE"
+    },
+    {
+      name = "speciality"
+      type = "STRING"
+      mode = "NULLABLE"
+    },
+    {
+      name = "faction"
+      type = "STRING"
+      mode = "NULLABLE"
+    },
+    {
+      name = "release_date"
+      type = "DATE"
+      mode = "NULLABLE"
+    },
+    {
+      name = "release_version"
+      type = "FLOAT64"
+      mode = "NULLABLE"
+    },
+  ])
+}
