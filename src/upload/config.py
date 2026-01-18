42df28ec80deb3f5bff147732be43161d620c266
vikram-parashar <vikramparashar24@gmail.com>
2026-01-18 23:57:51 +0530

    video discovered to bq

--- src/upload/config.py
+++ src/upload/config.py
@@ -4,5 +4,6 @@ BQ_LOCATION = "asia-south2"
 BQ_DATASET = "youtube_analytics"
 
 AGENT_TABLE = "dim_agent"
+VIDEO_TABLE = "dim_video"
 
 DISCOVERY_LOOKBACK_DAYS = 90
---
+++ src/upload/videos.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import pandas_gbq
+
+from config import BQ_DATASET,  PROJECT_ID, VIDEO_TABLE
+
+
+def main():
+    df = pd.read_csv("data/dim_videos.csv")
+    df["publish_date"] = pd.to_datetime(df["publish_date"])
+    df["discovered_at"] = pd.to_datetime(df["discovered_at"])
+
+    table_id = f"{BQ_DATASET}.{VIDEO_TABLE}"
+    pandas_gbq.to_gbq(df, table_id, project_id=PROJECT_ID)
+
+    print("Upload complete")
+
+
+if __name__ == "__main__":
+    main()
+
