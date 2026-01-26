a119923d60ef69b1ed31c9d502ba4f2034c794e1
vikram-parashar <vikramparashar24@gmail.com>
2026-01-26 20:11:42 +0530

    added more column to dim_vide and enriched

--- pipelines/discovery.py
+++ pipelines/discovery.py
@@ -100,6 +100,9 @@ def init_tables(con: DuckDBPyConnection):
            description VARCHAR,
            channel_id VARCHAR,
            published_at TIMESTAMP,
+           thumbnail VARCHAR,
+           tags VARCHAR[],
+           duration_seconds int,
            ingested_date DATE
         )
     """)
@@ -108,6 +111,9 @@ def init_tables(con: DuckDBPyConnection):
         CREATE TABLE IF NOT EXISTS dim_channel(
            channel_id VARCHAR PRIMARY KEY,
            channel_name VARCHAR,
+           thumbnail VARCHAR,
+           language VARCHAR,
+           country VARCHAR,
            ingested_date DATE
         )
     """)
--- src/upload/config.py
+++
@@ -1,9 +0,0 @@
-PROJECT_ID = "learning-474319"
-BQ_LOCATION = "asia-south2"
-
-BQ_DATASET = "youtube_analytics"
-
-AGENT_TABLE = "dim_agent"
-VIDEO_TABLE = "dim_video"
-
-DISCOVERY_LOOKBACK_DAYS = 90
