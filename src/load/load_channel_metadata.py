6467492b2cceb64b6d9ac64d233999f0b0af7ae7
vikram-parashar <vikramparashar24@gmail.com>
2026-02-11 22:45:45 +0530

    channel enrichment

---
+++ src/load/load_channel_metadata.py
@@ -0,0 +1,24 @@
+import logging
+from duckdb import DuckDBPyConnection
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+
+def update_channel_metadata(con: DuckDBPyConnection, df: pd.DataFrame):
+    if df.empty:
+        return
+
+    con.register("channel_tmp", df)
+
+    print(df)
+    logger.info("Updating dim_channel table")
+    con.execute("""
+        UPDATE dim_channel AS v
+        SET 
+            language = t.language,
+            country = t.country,
+            thumbnail = t.thumbnail
+        FROM channel_tmp AS t
+        WHERE v.channel_id = t.channel_id
+    """)
